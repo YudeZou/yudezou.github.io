b1d679b7ead09ee8370e9517b914045e5a19d8bd
yudezou <xdzouyd@gmail.com>
2024-10-27 20:13:48 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yudezou.github.io',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), partytown({ config: { forward: ['dataLayer.push'] }})]
 });
